4f06c5d066ed31ee9beb08eac8e636abc6f3aea3
Reese Kunz <reesekunz21@gmail.com>
2020-01-21 19:28:05 -0700

    added player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, player_name, current_room):
+        self.player_name = player_name
+        self.current_room = current_room
+
+    def __str__(self):
+        return f"{self.player_name} is currently in room: {self.current_room}"
--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+# The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes which point to the room in that respective direction
+
+
+class Room:
+    def __init__(self, room_name, room_description):
+        self.room_name = room_name
+        self.room_description = room_description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"Room name: {self.room_name}. Room description: {self.room_description}"
